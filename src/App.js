73dcb6de879a22cd764550602f143f4dc4b3ac8f
lookagit <simjanovic.luka@gmail.com>
2024-08-24 01:06:06 +0200

    Landing page with router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './containers/LandingPage';
 
 const App = () => (
   <div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
   </div>
 );
 
